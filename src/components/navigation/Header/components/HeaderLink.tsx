c2a9f5c0e708ce168557cff5affa94f3c20b0a4d
josericardopenase <josericardopenase@gmail.com>
2023-07-14 22:20:54 +0100

    Added path redirections to header

--- src/components/navigation/Header/components/HeaderLink.tsx
+++ src/components/navigation/Header/components/HeaderLink.tsx
@@ -16,6 +16,7 @@ import {
 	SimpleGrid,
 	Text,
 } from "@chakra-ui/react"
+import { useRouter } from "next/navigation"
 import React from "react"
 import { IoChevronDown } from "react-icons/io5"
 
@@ -25,15 +26,17 @@ interface props {
 	children?: React.ReactNode
 	popoverWidth?: number,
 	cols?: number
+	path?: string;
 }
 
-export default function HeaderLink({ title, active, children, popoverWidth = 350, cols = 1 }: props) {
+export default function HeaderLink({ title, active, children, popoverWidth = 350, cols = 1, path = '/home'}: props) {
 	const textColor = active ? "primary.500" : "gray.600"
+	const router = useRouter()
 
 	return (
 		<Popover placement="bottom-start">
 			<PopoverTrigger>
-				<Box display="flex" alignItems="center" gap="1" cursor="pointer" userSelect='none'>
+				<Box display="flex" alignItems="center" gap="1" cursor="pointer" userSelect='none' onClick={() => !children && router.push(path)}>
 					<Text color={textColor} fontWeight="bold">
 						{title}
 					</Text>
--- src/components/navigation/Header/components/HeaderLinkOption.tsx
+++ src/components/navigation/Header/components/HeaderLinkOption.tsx
@@ -1,19 +1,23 @@
 import { Box, Icon, Text, Center } from "@chakra-ui/react"
+import Link from "next/link"
 import React from "react"
 import { IconType } from "react-icons"
 
 interface props {
 	icon: IconType
 	title: string
+	path?: string
 }
 
-export default function HeaderLinkOption({ icon, title }: props) {
+export default function HeaderLinkOption({ icon, title, path = "home" }: props) {
 	return (
-		<Box w="full" display="flex" alignItems="center" gap='4' userSelect='none' p='2' _hover={{bgColor: "gray.100"}} borderRadius='lg'>
-			<Center fontSize='2xl' borderRadius="lg" color="white" bgColor="secondary.500" w="12" h="12" >
-				<Icon as={icon}></Icon>
-			</Center>
-			<Text color='gray.600' fontWeight='semibold'>{title}</Text>
-		</Box>
+		<Link href={path}>
+			<Box w="full" display="flex" alignItems="center" gap='4' userSelect='none' p='2' _hover={{bgColor: "gray.100"}} borderRadius='lg' cursor='pointer'>
+				<Center fontSize='2xl' borderRadius="lg" color="white" bgColor="secondary.500" w="12" h="12" >
+					<Icon as={icon}></Icon>
+				</Center>
+				<Text color='gray.600' fontWeight='semibold'>{title}</Text>
+			</Box>
+		</Link>
 	)
 }

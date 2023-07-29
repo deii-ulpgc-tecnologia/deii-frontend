d5c7a430f14bae554ef15e847b708379fa558e4a
Cristóbal José Jiménez Gómez <cristobal_jjg@hotmail.com>
2023-07-29 22:07:33 +0100

    Creación de la tarjeta de equipo
    En principio, está hecho:
    - La implementación corresponde con los diseños.
    - La variante isHonorific funciona correctamente.
    - La variante noTitle funciona correctamente. (Supongo)
    - La variante noNetworks funciona correctamente.

--- src/types/db/dummies.ts
+++ src/types/db/dummies.ts
@@ -3,7 +3,7 @@ import { Faq, FaqTag } from "./faq";
 import { PocketBaseModel, TagModel } from "./generics";
 import { News, NewsTag } from "./new";
 
-export const PocketBaseModelDummy : PocketBaseModel = {
+export const PocketBaseModelDummy: PocketBaseModel = {
     "id": "RECORD_ID",
     "collectionId": "BHKW36mJl3ZPt6z",
     "collectionName": "posts",
@@ -11,29 +11,44 @@ export const PocketBaseModelDummy : PocketBaseModel = {
     "updated": "2022-01-01 23:59:59.456Z",
 }
 
-export const TagModelDummy : TagModel = {
+export const TagModelDummy: TagModel = {
     ...PocketBaseModelDummy,
     name: "Dummy tag",
     color: "red",
 }
 
-export const SocialNetworkDummy: SocialNetwork = {
+export const SocialNetworkDummy1: SocialNetwork = {
     ...PocketBaseModelDummy,
     network: {
         icon: "AiFillInstagram",
-        name: "instagram"
+        name: "Instagram"
     },
     url: "https://youtu.be/dQw4w9WgXcQ"
 }
-
-export const DelegateDummy : Delegate = {
+export const SocialNetworkDummy2: SocialNetwork = {
+    ...PocketBaseModelDummy,
+    network: {
+        icon: "AiFillInstagram",
+        name: "GitHub"
+    },
+    url: "https://youtu.be/dQw4w9WgXcQ"
+}
+export const SocialNetworkDummy3: SocialNetwork = {
+    ...PocketBaseModelDummy,
+    network: {
+        icon: "AiFillInstagram",
+        name: "Youtube"
+    },
+    url: "https://youtu.be/dQw4w9WgXcQ"
+}
+export const DelegateDummy: Delegate = {
     ...PocketBaseModelDummy,
     name: "Ruben Santana",
-    avatar : "https://img.ecartelera.com/noticias/71900/71983-c.jpg",
-    rol : "Delegado",
+    avatar: "https://img.ecartelera.com/noticias/71900/71983-c.jpg",
+    rol: "Delegado",
     belongsPresidency: true,
     isHonorary: true,
-    socialNetworks: [SocialNetworkDummy]
+    socialNetworks: [/* SocialNetworkDummy1, SocialNetworkDummy2, SocialNetworkDummy3 */]
 }
 
 export const NewsTagDummy: NewsTag = {

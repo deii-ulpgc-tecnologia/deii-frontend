42bac6d210a7de733abfbe7f2454ee9aa7378dd2
Aileen <aileenpadrontorres511@gmail.com>
2023-08-08 01:33:22 +0100

    Actualización del FaqTagDummy para ponerle una foto. Se crea la tarjeta para el botón a falta del enlace correcto al hace click.

--- src/dcomponents/faqs/FaqTagCard/index.tsx
+++ src/dcomponents/faqs/FaqTagCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaqTag } from '../../../types/db/faq'
-import { Box } from '@chakra-ui/react'
+import { Button, Heading, Image, Text } from '@chakra-ui/react'
+import Link from "next/link"
 
 interface Props {
     obj : FaqTag
@@ -8,7 +9,30 @@ interface Props {
 
 function FaqTagCard({obj}: Props) {
     return (
-        <Box></Box>
+        <Button
+            boxSize={'20rem'}
+            flexDirection={'column'}
+            alignContent={'center'}
+            borderRadius={'2rem'}
+            bgColor={'white'}
+            borderColor={'light-gray'}
+            borderWidth={'.15rem'}
+            as={Link}
+            prefetch={false}
+            replace href={`/faqs/${obj.id}`}>
+            <Image
+                src={obj.icon}
+                alt={obj.name}
+                borderRadius={'100%'}
+                boxSize={'7rem'}
+                objectFit={'cover'}/>
+            <Heading
+                fontSize={'1.7em'}
+                pt={'1rem'}
+                color={'black'}>
+                {obj.name}
+            </Heading>
+        </Button>
     )
 }
 
